6be44745f5e68b02c66215b269d133f8c3698237
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-14 15:57:46 +0545

    push

--- 02Basic/myObject.ts
+++ 02Basic/myObject.ts
@@ -1,20 +1,26 @@
-const User =  {
-    name: "Abhishek",
-    email: "abhishek@example.com",
-    isActive: true
+// const User =  {
+//     name: "Abhishek",
+//     email: "abhishek@example.com",
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}){}
+// createUser({name: "Abhishek", isPaid: false})
+
+
+// function createCourse():{name: string, price: number}{
+//     return{name: "react.js", price: 333}
+// }
+
+// let newUser = {name: "Abhishek", isPaid: false}
+
+// createUser(newUser);
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
 }
-
-function createUser({name: string, isPaid: boolean}){}
-createUser({name: "Abhishek", isPaid: false})
-
-
-function createCourse():{name: string, price: number}{
-    return{name: "react.js", price: 333}
-}
-
-let newUser = {name: "Abhishek", isPaid: false}
-
-createUser(newUser);
 
 
 export {}
