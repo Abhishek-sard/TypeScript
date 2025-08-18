e9760620f223927fee857995ff6787605493410a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-18 11:01:37 +0545

    push

--- 02Basic/myObject.ts
+++ 02Basic/myObject.ts
@@ -16,11 +16,47 @@
 
 // createUser(newUser);
 
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean;
+// }
+
+// function createUser(user: User): User {
+//     return {name: user.name, email: user.email, isActive: user.isActive}
+// }
+
+// createUser({name: "", email: "", isActive: true});
+
+
+// export {}
+
+
 type User = {
+
+    readonly _id: string;
     name: string;
     email: string;
     isActive: boolean;
+    credcardDetails?: number;
 }
 
+let myUser: User = {
+    _id: "12345",
+    name: "h",
+    email: "h@example.com",
+    isActive: false
+}
 
-export {}
+type cardNumber = {
+    cardnumber: string
+}
+type cardDate = {
+cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+myUser.email = "h@newdomain.com"
+// myUser._id= "asa"
