f489f22b07e9b8ce34030ccdde175e5e611f8028
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-18 11:14:27 +0545

    push

---
+++ 02Basic/myArray.ts
@@ -0,0 +1,25 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+const allUsers: number[] = []
+heroPower.push(2)
+allUsers.push({name: "", isActive: true})
+
+const user: User = {
+    name: "John Doe",
+    isActive: true
+}
+
+const users: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+superHeros.push("Spiderman")
+heroPower.push(2)
